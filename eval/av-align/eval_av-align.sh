6d2e2caba80455b8e93fe0f0b41f5c5dab88511f
MP-HOI <166725131+MP-HOI@users.noreply.github.com>
2024-08-08 21:42:47 +0800

    update eval_av-align.sh

--- eval/av-align/eval_av-align.sh
+++ eval/av-align/eval_av-align.sh
@@ -4,4 +4,7 @@ input_video_dir=/workspace/data3/VGGSound/video_test
 input_wav_dir=/workspace/mnt/mm_data/libingliang/stable-audio-tools/new_VGGSound_step700k_output_condition_video_orcale_lufs
 # cache_path="./video_cache.json"
 
-python av-align.py --input_video_dir $input_video_dir --input_wav_dir $input_wav_dir --cache_path $cache_path
+python av-align.py \
+--input_video_dir $input_video_dir \
+--input_wav_dir $input_wav_dir \
+# --cache_path $cache_path
