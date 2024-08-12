9c66d4bce7cea1fd3de567704ecabee7881a639e
MP-HOI <166725131+MP-HOI@users.noreply.github.com>
2024-08-12 20:45:19 +0800

    update eval_av-align-specvqgan.sh

--- eval/av-align/eval_av-align-specvqgan.sh
+++ eval/av-align/eval_av-align-specvqgan.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-input_video_dir=/workspace/data3/VGGSound/video_test
+input_video_dir=/workspace/mnt/mm_data/libingliang/VGGSound/video_test
 input_wav_dir=/workspace/data2/specvqgan_generated_test
 # cache_path="./video_cache.json"
 
 python av-align.py \
 --input_video_dir $input_video_dir \
 --input_wav_dir $input_wav_dir \
---ext wav
+--ext flac
 # --cache_path $cache_path
